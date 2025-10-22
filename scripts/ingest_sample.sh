0bafe00262353548dafaadd22da97b0c0a7f5106
Anurag Mishra <doitmywayweb@gmail.com>
2025-10-22 20:54:38 +0530

    Simplify JSON creation in ingest_sample.sh by removing unnecessary escaping and using direct variable assignment

--- scripts/ingest_sample.sh
+++ scripts/ingest_sample.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# Read the content of the file and escape it for JSON
-CONTENT=$(cat sample_data/about_rag.txt | python3 -c 'import json,sys; print(json.dumps(sys.stdin.read()))')
-
-# Remove the leading and trailing quotes that python adds
-CONTENT="${CONTENT:1:-1}"
-
-# Create the JSON payload and send the request
-curl -X POST http://localhost:8000/ingest-text \
-  -H "Content-Type: application/json" \
-  -d "{\"content\":\"$CONTENT\"}"
+TEXT=$(cat sample_data/about_rag.txt)
+JSON="{\"content\":\"$TEXT\"}"
+echo $JSON | curl -X POST http://localhost:8000/ingest-text -H "Content-Type: application/json" -d @-
